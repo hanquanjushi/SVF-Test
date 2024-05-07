185954ccc5eb4e67cb55a09338ce0369e92bfa61
hanquanjushi <2982712752@qq.com>
2024-05-07 03:47:47 +0000

    add

--- svf-llvm/include/SYN/LightAnalysis.h
+++ svf-llvm/include/SYN/LightAnalysis.h
@@ -15,7 +15,7 @@
 
 namespace SVF
 {
-
+class Modification;
 class LightAnalysis
 {
 
@@ -48,14 +48,14 @@ public:
 
     static void printSourceRange(CXSourceRange range,
                                  const std::string& blockName);
-    
+
     static enum CXChildVisitResult printAST(CXCursor cursor, CXCursor parent,
                                             CXClientData clientData);
 
     static enum CXChildVisitResult cursorVisitor(CXCursor cursor,
                                                  CXCursor parent,
                                                  CXClientData client_data);
-    
+
     static enum CXChildVisitResult countChildren(CXCursor cursor,
                                                  CXCursor parent,
                                                  CXClientData clientData);
@@ -100,6 +100,97 @@ public:
 
     /// Only works for Intra, Call, Ret ICFGNodes.
     static const SVFInstruction* getInstFromICFGNode(const ICFGNode*);
+};
+
+class ReadWriteContext
+{
+
+    friend class Modification;
+
+private:
+    // 以传过来的 Modification 对应文件的绝对地址为基础，为每一个 File
+    // 都维持一个 context，最后一起 dump 到文件
+    std::string srcFilePath;
+
+    std::string oracleText;
+
+    std::string workText;
+
+    // std::unordered_map<>;
+
+    FILE* inFp = nullptr;
+
+    // 统一为在原来的文件名后面加个 "_synthesized"
+    // 为新文件名，放在同路径上，e.g. "a.c" => "a_synthesized.c".
+    FILE* outFp = nullptr;
+
+public:
+};
+
+class Modification
+{
+
+private:
+    /// 看看需要存些啥。
+
+    const ICFGNode* icfgNode;
+
+    int _mType;
+
+    std::string concreteReplacementStmt;
+
+public:
+    // TODO: Need some specific handling strategies for different types?
+    enum MType
+    {
+        Uninitialized,
+        Delete,
+        Replace,
+        New,
+        HoleFilling,
+    };
+
+    Modification();
+
+    Modification(MType, const std::string&, const ICFGNode*);
+
+    Modification(MType, const std::string&);
+
+    ~Modification();
+
+    // 下面函数用来设置 Modification 的状态，你可以想想这些有哪些需要存在类里面
+    // field 里的，
+
+    /// Delete（不处理 switch）.
+    // 把 branch 的其中一个分支删掉，保留另一个。
+    void deleteEitherBranch(const SVFValue* branchInst, bool condValue);
+
+    // 把 branch 所有分支全删掉，注意处理下 if else 情况。
+    void deleteBranch(const SVFValue* branchInst);
+
+    void deleteLoop(const SVFValue* inst);
+
+    void deleteStmt(const SVFValue* inst);
+
+    /// Replace.
+    void replace(const SVFValue* inst, std::string str);
+
+    /// New.
+    // 从 startInst 到 endInst 直接选一行插入。
+    void addNewCodeSnippet(const SVFValue* startInst, const SVFValue* endInst,
+                           std::string str);
+
+    void addNewCodeSnippetAfter(const SVFValue* startInst, std::string str);
+
+    void addNewCodeSnippetBefore(const SVFValue* endInst, std::string str);
+
+    /// HoleFilling.
+    // 可以理解为 hole 就是 "$"" + "holeNumber"，比如 $1, $2, $3,
+    // ...，直接字符串精准匹配，换成 varName。
+    void setHoleFilling(int holeNumber, std::string varName);
+
+    // 通过 inst 去找 ast 上这句话定义的那个变量名，然后精准替换。
+    void setHoleFilling(int holeNumber, const SVFValue* varDefInst);
 };
 
 } // namespace SVF
--- svf-llvm/tools/Synthesizer/test/Html-Parser/compile.py
+++ svf-llvm/tools/Synthesizer/test/Html-Parser/compile.py
@@ -2,15 +2,15 @@ import os
 import subprocess
 
 # 编译所有的C文件
-c_files = ["LinkedList.c", "main.c", "parser.c", "utils.c"]
-for file in c_files:
-    subprocess.run(["gclang", "-g", f"{file}.o", file])
+c_files = ["LinkedList", "main", "parser", "utils"]
+o_files = []
 
-# 提取LLVM bitcode
-bc_files = []
 for file in c_files:
-    subprocess.run(["get-bc", "-o", f"{file}.bc", f"{file}.o"])
-    bc_files.append(f"{file}.bc")
+    subprocess.run(["gclang", "-g", "-c", f"{file}.c", "-o", f"{file}.o"])
+    o_files.append(f"{file}.o")
 
-# 链接所有的LLVM bitcode文件
-subprocess.run(["llvm-link", "-o", "output.ll"] + bc_files)
+# 将所有的.o文件链接在一起
+subprocess.run(["gclang", "-g"] + o_files + ["-o", "my_program"])
+
+# 使用get-bc命令生成.bc文件
+subprocess.run(["get-bc", "-b", "my_program"])
