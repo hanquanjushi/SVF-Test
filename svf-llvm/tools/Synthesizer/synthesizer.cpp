329d5d97948f5c6d3b45be1bb98a934243902508
hanquanjushi <2982712752@qq.com>
2024-05-08 05:39:16 +0000

    add

--- svf-llvm/tools/Synthesizer/synthesizer.cpp
+++ svf-llvm/tools/Synthesizer/synthesizer.cpp
@@ -37,52 +37,54 @@ static Option<std::string> NEWSPECPATH("newspec",
 void traverseOnSVFStmt(const ICFGNode* node)
 {
     auto str = SOURCEPATH();
-    auto lightAnalysis = new LightAnalysis(str);
+   // auto lightAnalysis = new LightAnalysis(str);
     for (const SVFStmt* stmt : node->getSVFStmts())
     {
         std::string stmtstring = stmt->getValue()->toString();
-        if (const BranchStmt* branch = SVFUtil::dyn_cast<BranchStmt>(stmt))
-        {
-            std::string brstring = branch->getValue()->toString();
-            SVFVar* branchVar = const_cast<SVFVar*>(branch->getBranchInst());
-            SVFValue* branchValue =
-                const_cast<SVFValue*>(branchVar->getValue());
-            std::string location = branchValue->getSourceLoc();
-            if (location == "")
-            {
-                break;
-            }
+        std::cout << stmtstring << std::endl;
+      //  if (const BranchStmt* branch = SVFUtil::dyn_cast<BranchStmt>(stmt))
+       // {
+            /*        std::string brstring = branch->getValue()->toString();
+                   SVFVar* branchVar =
+               const_cast<SVFVar*>(branch->getBranchInst()); SVFValue*
+               branchValue = const_cast<SVFValue*>(branchVar->getValue());
+                   std::string location = branchValue->getSourceLoc();
+                   if (location == "")
+                   {
+                       break;
+                   }
 
-            std::string::size_type pos = location.find("\"ln\":");
-            unsigned int num = std::stoi(
-                location.substr(pos + 5, location.find(",") - pos - 5));
-            SVFVar* conditionVar = const_cast<SVFVar*>(branch->getCondition());
-            std::string conditionstring = conditionVar->getValue()->toString();
-            //   %5 = icmp slt i32 %4, 0, !dbg !17 { "ln": 7, "cl": 11, "fl":
-            //   "test1.c" }
-            std::size_t icmpPos = conditionstring.find("icmp");
-            if (icmpPos != std::string::npos)
-            {
-                std::size_t spacePos = conditionstring.find(" ", icmpPos);
-                std::size_t nextSpacePos =
-                    conditionstring.find(" ", spacePos + 1);
-                std::string operation = conditionstring.substr(
-                    spacePos + 1, nextSpacePos - spacePos - 1);
-                std::vector<std::string> parameters = {};
-                pos = conditionstring.find("\"fl\": \"");
-                if (pos == std::string::npos)
-                { 
-                    return;
-                }
-                std::string srcpath = conditionstring.substr(
-                    pos + 7, conditionstring.find("\" }") - pos - 7);
-                std ::cout << srcpath << std::endl;
-                lightAnalysis->findNodeOnTree(num, branch_order, operation,
-                                              parameters, srcpath);
-            }
-        }
+                   std::string::size_type pos = location.find("\"ln\":");
+                   unsigned int num = std::stoi(
+                       location.substr(pos + 5, location.find(",") - pos - 5));
+                   SVFVar* conditionVar =
+               const_cast<SVFVar*>(branch->getCondition()); std::string
+               conditionstring = conditionVar->getValue()->toString();
+                   //   %5 = icmp slt i32 %4, 0, !dbg !17 { "ln": 7, "cl": 11,
+               "fl":
+                   //   "test1.c" }
+                   std::size_t icmpPos = conditionstring.find("icmp");
+                   if (icmpPos != std::string::npos)
+                   {
+                       std::size_t spacePos = conditionstring.find(" ",
+               icmpPos); std::size_t nextSpacePos = conditionstring.find(" ",
+               spacePos + 1); std::string operation = conditionstring.substr(
+                           spacePos + 1, nextSpacePos - spacePos - 1);
+                       std::vector<std::string> parameters = {};
+                       pos = conditionstring.find("\"fl\": \"");
+                       if (pos == std::string::npos)
+                       {
+                           return;
+                       }
+                       std::string srcpath = conditionstring.substr(
+                           pos + 7, conditionstring.find("\" }") - pos - 7);
+                       std ::cout << srcpath << std::endl;
+                       lightAnalysis->findNodeOnTree(num, branch_order,
+               operation, parameters, srcpath);
+                   } */
+      //  }
 
-        else if (const AddrStmt* addr = SVFUtil::dyn_cast<AddrStmt>(stmt))
+        if (const AddrStmt* addr = SVFUtil::dyn_cast<AddrStmt>(stmt))
         {
             std::string addrstring = addr->getValue()->toString();
         }
@@ -123,39 +125,40 @@ void traverseOnSVFStmt(const ICFGNode* node)
         {
             std::string retstring = retPE->getValue()->toString();
         }
-        /*    else if (const CallPE* callPE = SVFUtil::dyn_cast<CallPE>(stmt))
-           {
-               std::string callstring = callPE->getValue()->toString();
-               std::cout << callstring << std::endl;
-               CallICFGNode* callNode =
-                   const_cast<CallICFGNode*>(callPE->getCallSite());
-               std::string callinfo = callNode->toString();
-               SVFInstruction* cs =
-                   const_cast<SVFInstruction*>(callNode->getCallSite());
-               std::string m = cs->getSourceLoc();
-               std::string::size_type pos = m.find("\"ln\":");
-               unsigned int num =
-                   std::stoi(m.substr(pos + 5, m.find(",") - pos - 5));
-               std::regex re("@(\\w+)\\((.+)\\)");
-               std::smatch match;
-               std::string functionName;
-               std::vector<std::string> parameters;
-               if (std::regex_search(callstring, match, re) && match.size() > 2)
-               {
-                   functionName = match.str(1);
-                   std::string parametersStr = match.str(2);
-                   std::stringstream ss(parametersStr);
-                   std::string parameter;
-                   while (std::getline(ss, parameter, ','))
-                   {
-                       parameter.erase(0, parameter.find_first_not_of(' '));
-                       parameter.erase(parameter.find_last_not_of(' ') + 1);
-                       parameters.push_back(parameter);
-                   }
-               }
-               lightAnalysis->findNodeOnTree(num, call_order, functionName,
-                                             parameters);
-           } */
+        else if (const CallPE* callPE = SVFUtil::dyn_cast<CallPE>(stmt))
+        {
+            std::string callstring = callPE->getValue()->toString();
+            std::cout << callstring << std::endl;
+            /*            CallICFGNode* callNode =
+                           const_cast<CallICFGNode*>(callPE->getCallSite());
+                       std::string callinfo = callNode->toString();
+                       SVFInstruction* cs =
+                           const_cast<SVFInstruction*>(callNode->getCallSite());
+                       std::string m = cs->getSourceLoc();
+                       std::string::size_type pos = m.find("\"ln\":");
+                       unsigned int num =
+                           std::stoi(m.substr(pos + 5, m.find(",") - pos - 5));
+                       std::regex re("@(\\w+)\\((.+)\\)");
+                       std::smatch match;
+                       std::string functionName;
+                       std::vector<std::string> parameters;
+                       if (std::regex_search(callstring, match, re) &&
+               match.size() > 2)
+                       {
+                           functionName = match.str(1);
+                           std::string parametersStr = match.str(2);
+                           std::stringstream ss(parametersStr);
+                           std::string parameter;
+                           while (std::getline(ss, parameter, ','))
+                           {
+                               parameter.erase(0, parameter.find_first_not_of('
+               ')); parameter.erase(parameter.find_last_not_of(' ') + 1);
+                               parameters.push_back(parameter);
+                           }
+                       }
+                       lightAnalysis->findNodeOnTree(num, call_order,
+               functionName, parameters); */
+        }
     }
 }
 
@@ -185,8 +188,8 @@ int main(int argc, char** argv)
     // instruction，对于 call instruction 你可以用 getOperand 拿到它的参数
     // 拿到basicblock:
     auto str = SOURCEPATH();
-    auto lightAnalysis = new LightAnalysis(str);
-    for (const SVFFunction* F : svfModule->getFunctionSet())
+    //auto lightAnalysis = new LightAnalysis(str);
+    /* for (const SVFFunction* F : svfModule->getFunctionSet())
     {
         std::string functionstring = F->toString();
         std::cout << functionstring << std::endl;
@@ -206,80 +209,78 @@ int main(int argc, char** argv)
             lightAnalysis->findNodeOnTree(num, define_order, functionName, {},
                                           srcpath);
         }
-        for (const SVFBasicBlock* bb : F->getBasicBlockList())
-        {
-            for (const SVFInstruction* inst : bb->getInstructionList())
-            {
-                std::string inststring = inst->toString();
-                // std ::cout << inststring << std::endl;
-                std::istringstream iss(inststring);
-                std::vector<std::string> words;
-                std::string word;
-                while (iss >> word)
-                {
-                    words.push_back(word);
-                }
-                int flag = 0;
+               for (const SVFBasicBlock* bb : F->getBasicBlockList())
+               {
+                   for (const SVFInstruction* inst : bb->getInstructionList())
+                   {
+                       std::string inststring = inst->toString();
+                       // std ::cout << inststring << std::endl;
+                       std::istringstream iss(inststring);
+                       std::vector<std::string> words;
+                       std::string word;
+                       while (iss >> word)
+                       {
+                           words.push_back(word);
+                       }
+                       int flag = 0;
 
-                if ((words[0] == "call" && words.size() >= 3 &&
-                     words[2] != "@llvm.dbg.declare(metadata") ||
-                    (words.size() >= 3 && words[2] == "call"))
-                {
-                    flag = 1;
-                }
-                if (flag == 0)
-                {
-                    continue;
-                }
+                       if ((words[0] == "call" && words.size() >= 3 &&
+                            words[2] != "@llvm.dbg.declare(metadata") ||
+                           (words.size() >= 3 && words[2] == "call"))
+                       {
+                           flag = 1;
+                       }
+                       if (flag == 0)
+                       {
+                           continue;
+                       }
 
-                if (flag == 1)
-                {
-                    //    std::cout << inststring << std::endl;
-                    std::string m = inst->getSourceLoc();
-                    //"{ \"ln\": 15, \"cl\": 12, \"fl\": \"test1.c\" }"
-                    if (m == "")
-                    {
-                        continue;
-                    }
-                    std::string::size_type pos = m.find("\"ln\":");
-                    unsigned int num =
-                        std::stoi(m.substr(pos + 5, m.find(",") - pos - 5));
-                    std::regex re("@(\\w+)\\((.+)\\)");
-                    std::smatch match;
-                    std::string functionName;
-                    pos = m.find("\"fl\": \"");
-                    std::string srcpath =
-                        m.substr(pos + 7, m.find("\" }") - pos - 7);
+                       if (flag == 1)
+                       {
+                           //    std::cout << inststring << std::endl;
+                           std::string m = inst->getSourceLoc();
+                           //"{ \"ln\": 15, \"cl\": 12, \"fl\": \"test1.c\" }"
+                           if (m == "")
+                           {
+                               continue;
+                           }
+                           std::string::size_type pos = m.find("\"ln\":");
+                           unsigned int num =
+                               std::stoi(m.substr(pos + 5, m.find(",") - pos -
+           5)); std::regex re("@(\\w+)\\((.+)\\)"); std::smatch match;
+                           std::string functionName;
+                           pos = m.find("\"fl\": \"");
+                           std::string srcpath =
+                               m.substr(pos + 7, m.find("\" }") - pos - 7);
 
-                    std::vector<std::string> parameters;
-                    if (std::regex_search(inststring, match, re) &&
-                        match.size() > 2)
-                    {
-                        functionName = match.str(1);
-                        std::string parametersStr = match.str(2);
-                        std::stringstream ss(parametersStr);
-                        std::string parameter;
-                        while (std::getline(ss, parameter, ','))
-                        {
-                            parameter.erase(0,
-                                            parameter.find_first_not_of(' '));
-                            parameter.erase(parameter.find_last_not_of(' ') +
-                                            1);
-                            parameters.push_back(parameter);
-                        }
-                    }
-                    if (functionName == "")
-                    {
-                        continue;
-                    }
-                    std::cout << functionName << std::endl;
-                    std ::cout << srcpath << std::endl;
-                    lightAnalysis->findNodeOnTree(num, call_order, functionName,
-                                                  parameters, srcpath);
-                }
-            }
-        }
-    }
+                           std::vector<std::string> parameters;
+                           if (std::regex_search(inststring, match, re) &&
+                               match.size() > 2)
+                           {
+                               functionName = match.str(1);
+                               std::string parametersStr = match.str(2);
+                               std::stringstream ss(parametersStr);
+                               std::string parameter;
+                               while (std::getline(ss, parameter, ','))
+                               {
+                                   parameter.erase(0,
+                                                   parameter.find_first_not_of('
+           ')); parameter.erase(parameter.find_last_not_of(' ') + 1);
+                                   parameters.push_back(parameter);
+                               }
+                           }
+                           if (functionName == "")
+                           {
+                               continue;
+                           }
+                           std::cout << functionName << std::endl;
+                           std ::cout << srcpath << std::endl;
+                           lightAnalysis->findNodeOnTree(num, call_order,
+           functionName, parameters, srcpath);
+                       }
+                   }
+               } 
+    }*/
     SVF::SVFIRBuilder builder(svfModule);
     auto pag = builder.build();
     assert(pag && "pag cannot be nullptr!");
